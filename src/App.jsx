1b3c0a3ddec178e1dec7b50a1affbb0b45c03379
atabakgp <atabakgp@gmail.com>
2025-04-16 22:08:42 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <div className='app'>
+        <h1>My React App</h1>
+        <p>This is a simple React application.</p>
+        <p>It uses Vite for fast development and build.</p>
+        <p>Enjoy coding!</p>
+        <button className='btn'>Click Me!</button>
+      </div>
+    </>
+  )
+}
+
+export default App
