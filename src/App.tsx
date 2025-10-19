8efcfb84f6595d9bc9d6441bcb0a1b0a9eb8417f
atabakgp <atabakgp@gmail.com>
2025-10-20 00:41:27 +0300

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router";
 import Header from "@/components/header/header";
-// import "bootstrap/dist/css/bootstrap.min.css";
 import useAuthObserver from "./hooks/auth/useAuthObserver";
 import Spinner from "@/components/spinner/spinner";
 import { useLoading } from "./context/LoadingContext";
