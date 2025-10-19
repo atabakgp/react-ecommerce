abf9ab0ae1c1b2c5ee85c13c646e209ea681ba23
atabakgp <atabakgp@gmail.com>
2025-10-20 01:09:04 +0300

    bug fix

--- src/routes/privateRoutes.tsx
+++ src/routes/privateRoutes.tsx
@@ -2,14 +2,14 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useUser } from "../context/UserContext";
 import { useLoading } from "../context/LoadingContext";
 
-import Spinner from "@/components/spinner/spinner";
+// import Spinner from "@/components/spinner/spinner";
 
 const PrivateRoute = () => {
   const { user } = useUser();
   const { loading } = useLoading();
 
   if (loading) {
-    return <Spinner />;
+    return "loading...";
   }
 
   return user ? <Outlet /> : <Navigate to="/login" />;
