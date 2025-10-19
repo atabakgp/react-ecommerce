6cf6108ee442393e95d04f5744abc07680c312d6
atabakgp <atabakgp@gmail.com>
2025-10-19 21:17:23 +0300

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 import Header from "@/components/header/header";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import useAuthObserver from "./hooks/auth/useAuthObserver";
-import Spinner from "@/components/spinner/Spinner";
+import Spinner from "@/components/spinner/spinner";
 import { useLoading } from "./context/LoadingContext";
 
 function App() {
